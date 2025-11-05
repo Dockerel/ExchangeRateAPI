7bf746ba2e311557f5fbafa4f0400f91901dd19f
Giheon Do <dgh0001@naver.com>
2025-11-05 15:42:06 +0900

    refactor: 오늘 환율만 표시

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import json, requests, pytz
+import json, requests, pytz, os
 import datetime as dt
 
-with open("data/data.json", "r") as f:
-    json_data = json.load(f)
+os.remove("data/data.json")
 
 url = "https://m.search.naver.com/p/csearch/content/qapirender.nhn?key=calculator&pkid=141&q=%ED%99%98%EC%9C%A8&where=m&u1=keb&u6=standardUnit&u7=0&u3=USD&u4=KRW&u8=down&u2=1"
 
@@ -16,11 +15,7 @@ current_time_kst = dt.datetime.now(kst)
 todayDate = str(current_time_kst).split()[0]
 
 new_data = {}
-new_data["date"] = todayDate
-new_data["rate"] = todayRate
-
-json_data["data"].append(new_data)
-del json_data["data"][0]
+new_data["exchange_rate"] = todayRate
 
 with open("data/data.json", "w", encoding="utf-8") as f:
-    json.dump(json_data, f, indent=2, ensure_ascii=False)
+    json.dump(new_data, f, indent=2, ensure_ascii=False)
